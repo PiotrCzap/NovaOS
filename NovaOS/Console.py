f7d0aec6173fd20f7e877338c6eff0a1fc053892
PiotrCzap <piotrczapmlawa@interia.pl>
2025-06-29 21:48:40 +0200

    NovaOS

---
+++ NovaOS/Console.py
@@ -0,0 +1,10 @@
+from main import *
+
+console_input = input("> ")
+print("NovaOS")
+
+if console_input == "help":
+    print("help - all system commands")
+    print("ver - system version")
+if console_input == "ver":
+    print(system_version)
---
+++ NovaOS/main.py
@@ -0,0 +1,26 @@
+import os
+import random
+import sys
+import time
+import Console
+
+system_version = "1.0"
+
+def main():
+    os.system("cls")
+    print("_")
+    time.sleep(0.5)
+    os.system("cls")
+    print("")
+    time.sleep(0.5)
+    os.system("cls")
+    print("_")
+    time.sleep(0.5)
+    os.system("cls")
+    print("")
+    time.sleep(0.5)
+    Console
+
+
+if __name__ == __name__:
+    main()

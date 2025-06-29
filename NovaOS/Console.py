eaabda8ff1ca335bc60e6a51bd1032ae67c7e86d
PiotrCzap <piotrczapmlawa@interia.pl>
2025-06-30 01:49:36 +0200

    NovaOS More Commands

--- NovaOS/Console.py
+++ NovaOS/Console.py
@@ -1,4 +1,7 @@
 from Settings import *
+import psutil
+import platform
+import GPUtil
 
 console_input = input("> ")
 print("NovaOS")
@@ -6,5 +9,12 @@ print("NovaOS")
 if console_input == "help":
     print("help - all system commands")
     print("ver - system version")
+    print("about - all system info")
 if console_input == "ver":
     print(system_version)
+if console_input == "about":
+    print("Version 1.0")
+    print(f"Memory: {memory / (1024 ** 2):.2f} MB")
+    print("CPU:", platform.processor())
+    print("CPU Cores: ", physical_cores)
+    print("CPU threads: ", logical_cores)
--- NovaOS/Settings.py
+++ NovaOS/Settings.py
@@ -1,1 +1,8 @@
+import psutil
+import GPUtil
+
+
+physical_cores = psutil.cpu_count(logical=False)
+logical_cores = psutil.cpu_count(logical=True)
+memory = psutil.virtual_memory().total
 system_version = "1.0"

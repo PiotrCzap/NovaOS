f3fe9ee82b164a30b409a1f687cfe7227db44cf0
PiotrCzap <piotrczapmlawa@interia.pl>
2025-07-02 20:08:11 +0200

    NovaOS Code Fixing

--- NovaOS/Console.py
+++ NovaOS/Console.py
@@ -6,28 +6,40 @@ import shutil
 
 console_input = input("> ")
 
-
-
-if console_input == "help":
+def help_command():
     print("help - all system commands")
     print("ver - system version")
     print("about - all system info")
     print("mktxt - making txt file")
 
-if console_input == "ver":
+def ver_command():
     print(system_version)
 
-if console_input == "about":
-    print("Version 1.0")
-    print(f"Memory: {memory / (1024 ** 2):.2f} MB")
-    print("CPU:", platform.processor())
-    print("CPU Cores: ", physical_cores)
-    print("CPU threads: ", logical_cores)
-if console_input == "mktxt":
+def mktxt_command():
     print("ENTER FILE NAME")
     txt_file_name = input()
     
     with open(txt_file_name, "w") as txt_file:
         txt_file.write("")
-        
+        shutil.move("Desktop")
+
+
+def about_command():
+    print("Version 1.0")
+    print(f"Memory: {memory / (1024 ** 2):.2f} MB")
+    print("CPU:", platform.processor())
+    print("CPU Cores: ", physical_cores)
+    print("CPU threads: ", logical_cores)
+
+if console_input == "help":
+    help_command()
+
+if console_input == "ver":
+    ver_command()
+
+if console_input == "about":
+    about_command()
+
+if console_input == "mktxt":
+    mktxt_command()
 

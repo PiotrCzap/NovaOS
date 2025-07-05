5da024900f0829767880fc22368e90d91fe66604
PiotrCzap <piotrczapmlawa@interia.pl>
2025-07-05 22:56:34 +0200

    NovaOS start menu button

--- tests/gui.py
+++ tests/gui.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -9,5 +10,11 @@ root.title()
 root.attributes("-fullscreen", True)
 task_bar_canvas = ctk.CTkCanvas(root, width=1920, height=25, bg="gray", highlightthickness=0)
 task_bar_canvas.pack(pady=1)
+
+#start_menu_image = ctk.CTkImage(
+#    dark_image=Image.open
+#)
+
+start_menu_button = ctk.CTkButton
 
 root.mainloop()

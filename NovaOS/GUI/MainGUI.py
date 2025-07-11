fa1b818ff77b26c37627a5effa7fe48144a87a7f
PiotrCzap <piotrczapmlawa@interia.pl>
2025-07-12 00:51:43 +0200

    NovaOS Poprawki

--- NovaOS/GUI/MainGUI.py
+++ NovaOS/GUI/MainGUI.py
@@ -3,13 +3,15 @@ from PIL import Image
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
- 
+
 system_font = "Arial"
 
 root = ctk.CTk()
 root.geometry("1920x1080+0+0")
 root.title()
 root.attributes("-fullscreen", True)
+task_bar_canvas = ctk.CTkCanvas(root, width=1920, height=28, bg="gray", highlightthickness=0)
+task_bar_canvas.pack(pady=1)
 
 # Desktop Taskbar Icon Images
 
@@ -17,17 +19,17 @@ start_menu_image = ctk.CTkImage(dark_image=Image.open("icons\Start menu icon.png
 power_menu_image = ctk.CTkImage(dark_image=Image.open("icons\Power Menu icon.png"))
 
 # Start Menu
-def Start_Menu():
 
-    start_menu_button = ctk.CTkButton(
-        master=root,
-        text="",
-        image=start_menu_image,
-        width=5,
-        height=5,
-        command=lambda: Settings_menu()
-    )
-    start_menu_button.place(x=0, y=1)
+start_menu_button = ctk.CTkButton(
+    master=root,
+    text="",
+    image=start_menu_image,
+    width=5,
+    height=5,
+    command=lambda: Settings_menu()
+)
+start_menu_button.place(x=0, y=1)
+
 
 # Settings Menu
 
@@ -83,7 +85,7 @@ def Settings_panel():
     Settings_window_tilebar.bind("<Button-1>", start_move)
     Settings_window_tilebar.bind("<ButtonRelease-1>", stop_move)
     Settings_window_tilebar.bind("<B1-Motion>", on_motion)
-
+    Settings_options_panel_system_button.pack(x=0, y=20)
 
 # Power Menu
 
@@ -112,7 +114,5 @@ power_menu_button = ctk.CTkButton(
 )
 
 power_menu_button.place(x=1892, y=1)
-
-
 
 root.mainloop()

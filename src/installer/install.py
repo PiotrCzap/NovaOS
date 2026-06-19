f850b2c87980edfc87ebd77ba7a02f7678215655
PiotrCzap <piotrczapmlawa@interia.pl>
2026-06-19 20:01:52 +0200

    first step installer makeing

--- src/installer/install.py
+++ src/installer/install.py
@@ -1,2 +1,16 @@
 import os
 import subprocess
+
+
+print("Preparing...")
+
+os.makedirs("root")
+os.makedirs("root/sys")
+os.makedirs("root/boot")
+os.makedirs("root/bin")
+os.makedirs("root/tmp")
+os.makedirs("root/run")
+os.makedirs("root/usr")
+os.makedirs("root/config")
+
+

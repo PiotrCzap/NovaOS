dc6146806f653af9cb233573d793680d94273515
PiotrCzap <piotrczapmlawa@interia.pl>
2026-06-22 23:48:30 +0200

    Little Changes in bootloader

--- src/boot/boot.c
+++ src/boot/boot.c
@@ -1,15 +1,22 @@
 #include "../drivers/lib.h"
 #include "../drivers/display/display.h"
 #include "../kernel/kernel.h"
+#include "../drivers/timer/timer.h"
 
 void init_boot()
 {
     printf("Loading Drivers...\n");
+    timer(1);
     printf("Loading Drivers [ " GREEN "OK" WHITE " ]\n");
+    timer(1);
     printf("Loading Kernel...\n");
+    timer(1);
     printf("Loading Kernel [ " GREEN "OK" WHITE " ]\n");
+    timer(1);
     printf("Loading Terminal...\n");
+    timer(1);
     printf("Loading Terminal [ " GREEN "OK" WHITE " ]\n");
+    timer(1);
     system("clear");
     init();
 }
